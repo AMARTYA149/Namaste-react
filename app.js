d895c5019b2d05ccd0128f57d2e4ef6c7f80d8c8
AMARTYA149 <amartya970914@gmail.com>
2024-09-21 19:48:35 +0530

    3rd commit

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const heading = React.createElement("h1", {
+const heading = React.createElement(
+  "h1",
+  {
     id: "heading",
-    xyz: "abc"
-}, "Hello from React!!!!");
+    xyz: "abc",
+  },
+  "Hello from React!!!!"
+);
 
 console.log("heading: ", heading);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 console.log("root: ", root);
 
