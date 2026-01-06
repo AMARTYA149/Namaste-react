005010cfdc13a9a33cbde690a6f738fa2deb381f
AMARTYA149 <amartya970914@gmail.com>
2026-01-06 08:10:02 +0530

    Ep-4 PaitPooja Dynamic Card wip-2

--- app.js
+++ app.js
@@ -23,81 +23,282 @@ import restaurantLogo from "./assets/img/restaurant_logo.png";
  * - Contact
  */
 
-const resData = {
-  type: "restaurant",
-  data: {
-    id: "382636",
-    name: "Wendy's Burgers",
-    cloudinaryImageId:
-      "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/11/097020c5-09e8-4430-96fb-77ef985c2935_382636.JPG",
-    locality: "Vasanth Nagar",
-    areaName: "R.T. Nagar",
-    costForTwo: "₹200 for two",
-    cuisines: ["Burgers", "American", "Fast Food", "Snacks"],
-    avgRating: 4.3,
-    parentId: "972",
-    avgRatingString: "4.3",
-    totalRatingsString: "3.4K+",
-    sla: {
-      deliveryTime: 31,
-      lastMileTravel: 3.9,
-      serviceability: "SERVICEABLE",
-      slaString: "30-35 mins",
-      lastMileTravelString: "3.9 km",
-      iconType: "ICON_TYPE_EMPTY",
-    },
-    availability: {
-      nextCloseTime: "2026-01-04 03:00:00",
-      opened: true,
-    },
-    badges: {},
-    isOpen: true,
-    type: "F",
-    badgesV2: {
-      entityBadges: {
-        imageBased: {},
-        textBased: {},
-        textExtendedBadges: {},
+const resList = [
+  {
+    type: "restaurant",
+    data: {
+      id: "43836",
+      name: "McDonald's",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/3/136c9e23-b373-45d5-9fad-7e4763ebd36b_43836.JPG",
+      locality: "MG Road",
+      areaName: "Ashok Nagar",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
+      avgRating: 4.3,
+      parentId: "630",
+      avgRatingString: "4.3",
+      totalRatingsString: "26K+",
+      sla: {
+        deliveryTime: 22,
+        lastMileTravel: 1.9,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.9 km",
+        iconType: "ICON_TYPE_EMPTY",
       },
-    },
-    aggregatedDiscountInfoV3: {
-      header: "ITEMS",
-      subHeader: "AT ₹59",
-    },
-    differentiatedUi: {
-      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-      differentiatedUiMediaDetails: {
-        lottie: {},
-        video: {},
+      availability: {
+        nextCloseTime: "2026-01-04 02:45:00",
+        opened: true,
       },
-    },
-    reviewsSummary: {},
-    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-    restaurantOfferPresentationInfo: {},
-    externalRatings: {
-      aggregatedRating: {
-        rating: "--",
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Ratnesh_Badges/Rx_Awards_2025/Bolt.png",
+            description: "Delivery!",
+          },
+        ],
       },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Ratnesh_Badges/Rx_Awards_2025/Bolt.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹99",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
     },
-    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
   },
-};
+  {
+    type: "restaurant",
+    data: {
+      id: "5938",
+      name: "Burger King",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/b88fe4d2-557a-42a5-a801-a43a40134004_5938.jpg",
+      locality: "Tasker Town",
+      areaName: "Shivaji Nagar",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.4,
+      parentId: "166",
+      avgRatingString: "4.4",
+      totalRatingsString: "40K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-01-04 05:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹48",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      id: "755276",
+      name: "La Pino'z Pizza",
+      cloudinaryImageId: "f44bc9708c514cd2dd6ae0d8b4677214",
+      locality: "Pulikeshi Nagar",
+      areaName: "Pulikeshi Nagar",
+      costForTwo: "₹250 for two",
+      cuisines: ["Pizzas", "Pastas", "Italian", "Desserts", "Beverages"],
+      avgRating: 4.3,
+      parentId: "4961",
+      avgRatingString: "4.3",
+      totalRatingsString: "3.6K+",
+      sla: {
+        deliveryTime: 33,
+        lastMileTravel: 4.5,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "4.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-01-04 02:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹79",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.0",
+          ratingCount: "188",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      id: "382636",
+      name: "Wendy's Burgers",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/11/097020c5-09e8-4430-96fb-77ef985c2935_382636.JPG",
+      locality: "Vasanth Nagar",
+      areaName: "R.T. Nagar",
+      costForTwo: "₹200 for two",
+      cuisines: ["Burgers", "American", "Fast Food", "Snacks"],
+      avgRating: 4.3,
+      parentId: "972",
+      avgRatingString: "4.3",
+      totalRatingsString: "3.4K+",
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 3.9,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "3.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-01-04 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹59",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+];
 
 const RestaurantCard = (props) => {
   const { resData } = props;
-  console.log("resData: ", resData);
+  const {cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla} = resData?.data;
+  
   return (
     <div className="card-restaurant">
       <img
         className="logo-restaurant"
         alt="Restaurant Logo"
-        src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+resData.data.cloudinaryImageId}
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
       />
-      <h3>{resData.data.name}</h3>
-      <h4>{resData.data.cuisines.join(', ')}</h4>
-      <h4>{resData.data.avgRating} ⭐️</h4>
-      <h4>{resData.data.costForTwo}</h4>
-      <h4>{resData.data.sla.slaString}</h4>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} ⭐️</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.slaString}</h4>
     </div>
   );
 };
@@ -108,14 +309,8 @@ const Body = () => {
       <div className="search">Search</div>
       <div className="container-restaurant">
         {/* Restaurant Cards */}
-        <RestaurantCard
-        //   resName="Pizza Street"
-        //   cuisines="Italian, Greek"
-        //   eta="38mins"
-        //   stars="4.8 ⭐️"
-        resData={resData}
-        />
-       
+        {resList.map((restaurant) => <RestaurantCard key={restaurant.data.id} resData={restaurant}/>)}
+      
       </div>
     </div>
   );
